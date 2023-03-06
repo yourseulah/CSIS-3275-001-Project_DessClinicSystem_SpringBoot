d7f46fb484f7996ff1b867000a26abb5a18364fb
David Salazar <dssalaza@gmail.com>
2023-03-06 15:37:59 -0800

    Added Invoice model

--- DessClinicSystem/src/main/java/com/example/dcs/DessClinicSystemApplication.java
+++ DessClinicSystem/src/main/java/com/example/dcs/DessClinicSystemApplication.java
@@ -13,6 +13,9 @@ import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
 import com.example.dcs.model.Doctor;
 import com.example.dcs.model.Doctor.Gender;
 import com.example.dcs.model.DoctorRepository;
+import com.example.dcs.model.Invoice;
+import com.example.dcs.model.Invoice.Status;
+import com.example.dcs.model.InvoiceRepository;
 
 @SpringBootApplication
 public class DessClinicSystemApplication {
@@ -36,10 +39,25 @@ public class DessClinicSystemApplication {
 		doctorRepository.findAll().forEach(System.out::println);
 	}
 	
+	
+	private void loadData(InvoiceRepository invoiceRepository) {
+		ArrayList<Invoice> invoices = new ArrayList<>();
+		invoiceRepository.save(
+				new Invoice(LocalDate.now(), "VISA", 120, Status.Paid, "GCG Canada", 10));
+		invoiceRepository.save(
+				new Invoice(LocalDate.now(), "AMEX", 40, Status.Pending, "GCG USA", 20));
+		invoiceRepository.save(
+				new Invoice(LocalDate.now(), "MASTERCARD", 1000, Status.Unknown, "GCG COL", 5));
+		
+		invoiceRepository.findAll().forEach(System.out::println);
+	}
+	
+	
 	@Bean
-	ApplicationRunner init(DoctorRepository doctorRepository) {
+	ApplicationRunner init(DoctorRepository doctorRepository, InvoiceRepository invoiceRepository) {
 		return args -> {
 			loadData(doctorRepository);
+			loadData(invoiceRepository);
 		};
 	}
 }
---
+++ DessClinicSystem/src/main/java/com/example/dcs/model/Invoice.java
@@ -0,0 +1,119 @@
+package com.example.dcs.model;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "invoice")
+public class Invoice {
+	
+	public enum Status {
+	    Paid,
+	    Pending,
+	    Unknown
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@Column(name = "payment_date")
+	private LocalDate paymentDate;
+	
+	@Column(name = "method")
+	private String method;
+	
+	@Column(name = "amount")
+	private double amount;
+	
+	@Column(name = "status")
+	@Enumerated(EnumType.STRING)
+	private Status status;
+		
+	@Column(name = "insurance_company")
+	private String insuranceCompany;
+	
+	@Column(name = "years_of_practice")
+	private int yearsOfPractice;
+	
+	
+
+	public Invoice() {
+
+	}
+
+	public Invoice(LocalDate paymentDate, String method, double amount, Status status, String insuranceCompany,
+			int yearsOfPractice) {
+		this.paymentDate = paymentDate;
+		this.method = method;
+		this.amount = amount;
+		this.status = status;
+		this.insuranceCompany = insuranceCompany;
+		this.yearsOfPractice = yearsOfPractice;
+	}
+
+	public long getdId() {
+		return id;
+	}
+
+	public void setdId(long id) {
+		this.id = id;
+	}
+
+	public LocalDate getPaymentDate() {
+		return paymentDate;
+	}
+
+	public void setPaymentDate(LocalDate paymentDate) {
+		this.paymentDate = paymentDate;
+	}
+
+	public String getMethod() {
+		return method;
+	}
+
+	public void setMethod(String method) {
+		this.method = method;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public String getInsuranceCompany() {
+		return insuranceCompany;
+	}
+
+	public void setInsuranceCompany(String insuranceCompany) {
+		this.insuranceCompany = insuranceCompany;
+	}
+
+	public int getYearsOfPractice() {
+		return yearsOfPractice;
+	}
+
+	public void setYearsOfPractice(int yearsOfPractice) {
+		this.yearsOfPractice = yearsOfPractice;
+	}
+
+
+	
+
+}
