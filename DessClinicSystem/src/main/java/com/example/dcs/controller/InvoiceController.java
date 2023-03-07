3ef265bbd6fbfd02e9bc536dec661de2038ba155
David Salazar <dssalaza@gmail.com>
2023-03-06 21:08:04 -0800

    Added invoice creation route

--- DessClinicSystem/src/main/java/com/example/dcs/controller/InvoiceController.java
+++ DessClinicSystem/src/main/java/com/example/dcs/controller/InvoiceController.java
@@ -8,14 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.dcs.model.Invoice;
@@ -39,6 +37,25 @@ public class InvoiceController {
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
+	
+    @PostMapping("/invoices")
+    public ResponseEntity<Invoice> createCountry(@RequestBody Invoice invoice){
+   	 try {
+   		 Invoice newInvoice = new Invoice(
+   				 invoice.getPaymentDate(), 
+   				 invoice.getMethod(), 
+   				 invoice.getAmount(), 
+   				 invoice.getStatus(), 
+   				 invoice.getInsuranceCompany(), 
+   				 invoice.getYearsOfPractice());
+   		 
+   		invoiceRepository.save(newInvoice);
+   		 
+   		 return new ResponseEntity<>(newInvoice,HttpStatus.CREATED);
+   	 }catch (Exception e) {
+   		 return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+   	 }
+    }
 	
 	@GetMapping("/invoices/{id}")
 	public ResponseEntity<Invoice> getInvoiceById(@PathVariable("id") long id) {
